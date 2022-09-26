7acd4a26acd5bcd80c62c2cf717a6b62f4c872e3
franciscosalesteixeira <franciscosalesteixeira88@ua.pt>
2022-09-27 00:15:34 +0100

    twitter stream v1.1 about to be deprecated: updated to v2

--- bot.js
+++ bot.js
@@ -17,6 +17,33 @@ const quotebot = new Twit({
   timeout_ms: 60 * 1000
 });
 
+// Open a realtime stream of Tweets, filtered according to rules
+// https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
+
+const needle = require('needle');
+require("dotenv").config();
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.BEARER_TOKEN;
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+
+// this sets up two rules - the value is the search terms to match on, and the tag is an identifier that
+// will be applied to the Tweets return to show which rule they matched
+// with a standard project with Basic Access, you can add up to 25 concurrent rules to your stream, and
+// each rule can be up to 512 characters long
+
+// Edit rules as desired below
+const rules = [{
+        'value': '@bladerunnersbot',
+    },
+];
+
+const endpointURL = "https://api.twitter.com/2/tweets?ids=";
+
 const request = require('request')
 const url = 'url'
 
@@ -44,13 +71,33 @@ setInterval(function () {
 // access render's deployed website every 14 min for the bot to always be available
 }, 1000 * 60 * 14);
 
-// Setting up a user stream
-const stream = quotebot.stream('statuses/filter', { 
-  track: "account" 
-});
+async function getRequest(id) {
 
-// looking for Tweet events
-stream.on('tweet', pressStart);
+    // These are the parameters for the API request
+    // specify Tweet IDs to fetch, and any additional fields that are required
+    // by default, only the Tweet ID and text are returned
+    const params = {
+        "ids": id, // Edit Tweet IDs to look up
+        "tweet.fields": "conversation_id", 
+        "user.fields": "username",
+        "expansions": "author_id" // Edit optional query parameters here
+    }
+
+    // this is the HTTP header that adds bearer token authentication
+    const res = await needle('get', endpointURL, params, {
+        headers: {
+            "User-Agent": "v2TweetLookupJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
 
 function getLine() {
 
@@ -65,30 +112,151 @@ function getLine() {
   return randomLine;
 }
 
-function pressStart(tweet) {
+async function getAllRules() {
 
-  let id = tweet.id_str;
-  let text = tweet.text;
-  let name = tweet.user.screen_name;
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
 
-  quotebot.get('statuses/show/:id', { 
-    id: tweet.in_reply_to_status_id_str 
-  });
+    if (response.statusCode !== 200) {
+        console.log("Error:", response.statusMessage, response.statusCode)
+        throw new Error(response.body);
+    }
 
-  if (text.includes("account") && (text.includes("quote") || text.includes("Quote") || text.includes("QUOTE"))) {
+    return (response.body);
+}
 
-    // get line from text file and compose corresponding tweet
-    let line = getLine();
-    let replyText = ("@" + name + " " + line);
+async function deleteAllRules(rules) {
 
-    // reply to sender
-    quotebot.post('statuses/update', { 
-      status: replyText, in_reply_to_status_id: id
+    if (!Array.isArray(rules.data)) {
+        return null;
+    }
+
+    const ids = rules.data.map(rule => rule.id);
+
+    const data = {
+        "delete": {
+            "ids": ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+async function setRules() {
+
+    const data = {
+        "add": rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 201) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+function streamConnect(retryAttempt) {
+
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
     });
 
-    console.log("replied to quote request!");
+    stream.on('data', data => {
+        try {
+            const json = JSON.parse(data);
 
-  } else {
-    console.log("quote was not triggered!");
-  };
+            lowercase = json.data['text'].toString().toLowerCase();
+            getRequest(json.data['id']).then(function(result) {
+
+              let reply_id = result.data[0]['conversation_id'];
+              let username = result.includes['users'][0]['username']; // "Some User token"
+
+              if (lowercase.includes('quote')) {
+                let line = getLine();
+                quotebot.post('statuses/update', {
+                   in_reply_to_status_id: reply_id, status: '@' + username + " " + line
+                }, (err, data, response) => {
+                  console.log(data)
+                })   
+              }
+                
+            })
+            
+            // A successful connection resets retry count.
+            retryAttempt = 0;
+        } catch (e) {
+            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                console.log(data.detail)
+                process.exit(1)
+            } else {
+                // Keep alive signal received. Do nothing.
+            }
+        }
+    }).on('err', error => {
+        if (error.code !== 'ECONNRESET') {
+            console.log(error.code);
+            process.exit(1);
+        } else {
+            // This reconnection logic will attempt to reconnect when a disconnection is detected.
+            // To avoid rate limits, this logic implements exponential backoff, so the wait time
+            // will increase if the client cannot reconnect to the stream. 
+            setTimeout(() => {
+                console.warn("A connection error occurred. Reconnecting...")
+                streamConnect(++retryAttempt);
+            }, 2 ** retryAttempt)
+        }
+    });
+
+    return stream;
+
 }
+
+
+(async () => {
+    let currentRules;
+
+    try {
+        // Gets the complete list of rules currently applied to the stream
+        currentRules = await getAllRules();
+
+        // Delete all rules. Comment the line below if you want to keep your existing rules.
+        await deleteAllRules(currentRules);
+
+        // Add rules to the stream. Comment the line below if you don't want to add new rules.
+        await setRules();
+
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+
+    // Listen to the stream.
+    streamConnect(0);
+})();
